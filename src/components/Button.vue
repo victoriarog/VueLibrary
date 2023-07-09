ae7a28a1ce7f9fc78f10996764c0c703ae621003
Vicka <vicrogatinskaya@gmail.com>
2023-07-09 04:09:14 +0200

    first commit

---
+++ src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+    <button 
+        :style="{
+        backgroundColor: hovered ? '#0070d2' : '#009ee3',
+        // color: hovered ? '#fff' : '#333'
+        }"
+        class="custom-button"
+        @mouseenter="hovered = true"
+        @mouseleave="hovered = false"
+    >
+        <slot></slot>
+    </button>
+    
+    </template>
+    
+    <script>
+    export default {
+    data() {
+        return {
+        hovered: false
+        };
+    }
+    };
+    </script>
+    
+    <style scoped>
+    .custom-button {
+    padding: 10px;
+    border: none;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    }
+    </style>
+    
+    
---
+++ src/components/TextInput.vue
@@ -0,0 +1,32 @@
+<template>
+    <input
+      type="text"
+      :style="{
+        backgroundColor: hovered ? '#0070d2' : '#009ee3'
+      }"
+      class="custom-input"
+      v-model="inputValue"
+      @mouseenter="hovered = true"
+      @mouseleave="hovered = false"
+    />
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        inputValue: '',
+        hovered: false
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .custom-input {
+    padding: 10px;
+    border: 1px solid;
+    transition: border-color 0.3s ease;
+  }
+  </style>
+  
