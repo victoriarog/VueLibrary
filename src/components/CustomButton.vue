d21411125c7a04e58fe3889dc5681a4ac4f1bd68
Vicka <vicrogatinskaya@gmail.com>
2023-07-10 01:36:49 +0200

    hover is css now in button

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -1,38 +1,33 @@
 <template>
-<button 
-    :style="{
-    backgroundColor: hovered ? '#0070d2' : '#009ee3'
-    }"
-    class="custom-button"
-    @mouseenter="hovered = true"
-    @mouseleave="hovered = false"
-    @click="handleClick"
->
-    <slot></slot>
-</button>
-
-</template>
-
-<script>
-export default {
+    <button
+      class="custom-button"
+      @click="handleClick"
+    >
+      <slot></slot>
+    </button>
+  </template>
+  
+  <script>
+  export default {
     name: 'CustomButton',
-    data() {
-        return {
-            hovered: false
-    };
-    },
     methods: {
-    handleClick() {
-      //onClick event
+      handleClick() {
+        // onClick event
+      }
     }
+  };
+  </script>
+  
+  <style scoped>
+  .custom-button {
+    padding: 10px;
+    border: none;
+    background-color: #009ee3;
+    transition: background-color 0.3s ease;
   }
-};
-</script>
-
-<style scoped>
-.custom-button {
-padding: 10px;
-border: none;
-}
-</style>
-
+  
+  .custom-button:hover {
+    background-color: #0070d2;
+  }
+  </style>
+  
